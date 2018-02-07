3871e2372c379c15d5f6258307a8ac0cd37473b7
HaRu <vharugun9093@gmail.com>
2018-02-07 15:09:11 +0100

    Practice of ball
    demo of katamori damacy

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public float ballSpeed;
+    private Rigidbody rb;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+            rb.AddForce(Vector3.forward * ballSpeed);
+
+        if (Input.GetKey("a"))
+            rb.AddForce(Vector3.left * ballSpeed);
+
+        if (Input.GetKey("s"))
+            rb.AddForce(Vector3.back * ballSpeed);
+
+        if (Input.GetKey("d"))
+            rb.AddForce(Vector3.right * ballSpeed);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Pickupable")
+            return;
+
+        bool hitMainCollider = false;
+
+        foreach(ContactPoint contact in col.contacts)
+        {
+            hitMainCollider = true;
+            break;
+        }
+        
+        if (hitMainCollider)
+        {
+            //Destroy(col.gameObject.GetComponent<Collider>());
+            //Destroy(col.gameObject.GetComponent<Rigidbody>());
+
+            Vector3 minSize = col.gameObject.GetComponent<Collider>().bounds.min;
+            Debug.Log(minSize);
+            Vector3 maxSize = col.gameObject.GetComponent<Collider>().bounds.max;
+            Debug.Log(maxSize);
+            col.transform.parent = transform;
+        }
+    }
+}
