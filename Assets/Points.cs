9534d7ec95178d0305deb44c0d97a6c886363cf8
Brent Minkes <brentminkes@hotmail.com>
2018-02-08 10:44:10 +0100

    need point system

---
+++ Assets/Points.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points {
+
+    public string category;
+    public int points;
+
+  /*  List<KeyValuePair<string, int>> type = new List<KeyValuePair<string, int>>()
+    {
+        new KeyValuePair<string, int>("Physiological",0),
+        new KeyValuePair<string, int>("Safety",0),
+        new KeyValuePair<string, int>("Connection",0),
+        new KeyValuePair<string, int>("Self Esteem",0),
+        new KeyValuePair<string, int>("Self Actualization",0),
+    };
+
+
+    List<int> limits = new List<int>()
+    {
+        100,200,300
+    };
+    
+
+    public void AddPoints(int index, int value)
+    {
+        string name = type[index].Key;
+        type[index].Value = value;
+    }*/
+    
+}
